0a6d87152162cf391c1a0e90aca963d3167656ac
Helen Pulcini <helenpulcini@gmail.com>
2023-02-04 14:01:21 +0000

    finished

--- crawl.js
+++ crawl.js
@@ -1,23 +1,43 @@
 const { JSDOM } = require("jsdom")
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {  //this way you don't crawl over the same page twice
+        return pages  
+    }
+
+    const normalisedCurrentURL = normaliseURL(currentURL)
+    if (pages[normalisedCurrentURL] > 0) {
+        pages[normalisedCurrentURL]++
+        return pages
+    }
+
+    pages[normalisedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL}`);
 
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-            return  //stop crawling
+            return pages
         }
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`non html response: ${contentType} on page: ${currentURL}`);
-            return
+            return pages
         }
-        console.log(await resp.text());
+        const htmlBody = await resp.text();
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (error) {
         console.log(`error in fetch: ${error.message} on page: ${currentURL}`);
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require("./crawl.js")
 
-function main() {
+async function main() {
    if (process.argv.length < 3) { //< 3 because 1 is interpreter 2 is program 3 is argument  e.g ~/java/Spider$ website.com
     console.log("no website provided");
     process.exit(1)
@@ -12,7 +12,12 @@ function main() {
    const baseURL = process.argv[2]
 
    console.log(`starting crawl of ${baseURL}`);
-   crawlPage(baseURL)
+   const pages = await crawlPage(baseURL, baseURL, {}) //the first url, the next url, an empty array of pages
+
+   for (const page of Object.entries(pages)) { //unless you make pages an object you cannot iterate through it
+    console.log(page);
+    
+   }
 }
 
 main()
