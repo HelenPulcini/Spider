8100dbc6ab4a6e7c985a69c6ae286c7953da6aca
Helen Pulcini <helenpulcini@gmail.com>
2023-02-04 13:43:09 +0000

    crawling pages

--- crawl.js
+++ crawl.js
@@ -1,5 +1,25 @@
 const { JSDOM } = require("jsdom")
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
+            return  //stop crawling
+        }
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response: ${contentType} on page: ${currentURL}`);
+            return
+        }
+        console.log(await resp.text());
+    } catch (error) {
+        console.log(`error in fetch: ${error.message} on page: ${currentURL}`);
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -36,5 +56,6 @@ function normaliseURL(urlString) {
 
 module.exports = {
     normaliseURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
